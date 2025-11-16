e2250adb2780f076560d5b6b43cd2e780081c4ec
Jay Thale <jaythale02@gmail.com>
2025-11-16 14:40:18 +0530

    cantroller

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -168,8 +168,7 @@ exports.loginCustomer = asyncHandler(async (req, res) => {
 
     res.cookie("Cutomer", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",    // localhost साठी
+        secure: true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         path: "/",
     });
